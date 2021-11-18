6bc4f216b338a96a8131080c0980eb1fd9faa169
Harshwardhan431 <81025973+Harshwardhan431@users.noreply.github.com>
2021-11-18 21:17:46 +0530

    initial commit

---
+++ android/app/src/main/kotlin/com/example/tech4bit/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.tech4bit
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:tech4bit/api.dart';
+import 'package:tech4bit/start.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Start(),
+    );
+  }
+}
